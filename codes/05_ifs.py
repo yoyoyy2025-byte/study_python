bec66098fc5b018d1fb6e50cb51c6b9fbf34bddb
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-17 18:03:10 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,16 @@
+first = 4
+
+# if first >= 5 :
+#     print("welcome to the rollercoaster!")
+#     print("it's me!")
+
+second = 6
+
+first = int(input("input first number: "))
+second = int(input("input second number: "))
+if first >= second : 
+    print("welcome to the rollercoaster!")
+    print("it's me!")
+
+
+print("End of program")

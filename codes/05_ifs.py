70bf6b0d9113f3fb6af0fe6a3abc54760d9891f0
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-18 17:29:08 +0900

    21321123

--- codes/05_ifs.py
+++ codes/05_ifs.py
@@ -1,17 +1,36 @@
-first = 4
+
+
+#first = 4
 
 # if first >= 5 :
 #     print("welcome to the rollercoaster!")
 #     print("it's me!")
 
-second = 6
+#second = 6
 
-first = int(input("input first number: "))
+#first = int(input("input first number: "))
 
-second = int(input("input second number: "))
-if first >= second : 
-    print("welcome to the rollercoaster!")
-    print("it's me!")
+#second = int(input("input second number: "))
+#if first >= second : 
+ #   print("welcome to the rollercoaster!")
+ #   print("it's me!")
 
 
-print("End of program")
+#print("End of program")
+
+## 다중 if-elif-flse 문
+#age = input("Enter your age")
+#if age < 4 : 
+#    print("Your ticket is free!")
+#elif age < 18 :
+#    print("Your ticket is $5.")
+#else:
+#    print("Your ticket is $10.")
+
+#print("End of program.")
+
+pass
+fourth = "welcome to python!"
+fourth_word = "python"
+if fourth_word ="python" in "welcome to python!" :
+    print("Yes, 'python' is here!")

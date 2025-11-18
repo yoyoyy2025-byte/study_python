bef1c0e507e1713ba95416c07fdd8cfd5e83ea10
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-18 11:59:11 +0900

    12321

---
+++ codes/05_if.py
@@ -0,0 +1,12 @@
+실습 if 문
+first = 4
+# if first >= 5 :
+#     print("welcome to the rollercoaster!")
+#     print("it's me!")
+second = 6
+first = int(input("input first number: "))
+second = int(input("input second number: "))
+if first >= second :
+    print("welcome to the rollercoaster!")
+    print("it's me!")
+print("End of program.")

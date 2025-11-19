6819c609424e4fce68c5320e38a0668dad3b0861
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-19 14:53:07 +0900

    12312

---
+++ quests/08_functions_debuggins_more.py
@@ -0,0 +1,57 @@
+#문제6
+
+#def to_celsius(temp):
+#    celsiu = (temp - 32) * 5 / 9   # 오타
+#    return celsiu
+
+#print(to_celsius(77))
+
+
+#문제 7
+
+#def to_celsius(temp):
+#    if temp > 0:
+#        celsius = (temp - 32) * 5 / 9
+#    return celsius            # 잘못된 위치
+        
+
+#print(to_celsius(50))
+
+#문제8
+
+#def to_celsius(temp):
+#    celsius = (temp - 3) * 5 / 9
+#    return celsius
+
+#temp = 77
+#result1 = to_celsius(temp)
+
+#temp = 95
+#result2 = to_celsius(temp)    # 오류: 인자 없음
+
+#temp = 50
+#result3 = to_celsius(temp)
+
+#print(result1, result2, result3)
+
+
+#9번 문제
+#def to_celsius(temp):
+#    return (temp - 32) * 5 / 9
+
+#temps = [77, 95, 50]
+
+#value = [to_celsius(t) for t in temps]   # 리스트의 각 요소에 함수를 적용
+#print(value)
+
+#10번 문제
+
+#def to_celsius(temp):
+#    return (temp - 32) * 5 / 9
+
+#if to_celsius(80) > 20:       # 함수 호출 누락
+#    print("warm")
+#else:
+#    print("cold")
+
+

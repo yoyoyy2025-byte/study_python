a65c030ccfb6e3e90c87fff28005ad9131b7b6d3
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-20 10:02:24 +0900

    21321

--- codes/08_functions_parameters.py
+++ codes/08_functions_parameters.py
@@ -29,5 +29,50 @@ math_scores = [88, 78, 68, 58, 48]
 length = len(kor_scores)
 len_list = range(length)
 
-range(len(kor_scores))
-pass
+# range(len(kor_scores))
+# pass
+# for i in range(len(kor_scores)):
+#     kor = kor_scores[i]
+#     eng = eng_scores[i]
+#     math = math_scores[i]
+#     sum = get_sum(kor, eng, math)
+#     print{f"{i+1}번 학생 총점: {sum}"}
+
+
+
+def get_for_sum(korean_scores, english_scores, mathematics_scores):
+    #실행할 코드
+    for i in range(len(korean_scores)):
+        kor = korean_scores[i]
+        eng = english_scores[i]
+        math = mathematics_scores[i]
+        sum = get_sum(kor, eng, math)
+        print(f"{i+1}번 학생 총점: {sum}")
+
+    return 0
+
+get_for_sum(kor_scores, eng_scores, math_scores)
+
+
+# 실습 코드
+# # sum = kor + eng
+# def get_sum(korean, english, mathatics=0):
+#     # 실행할 코드
+#     summation = korean + english + mathatics
+#     return summation
+# # for문 함수 작성
+# kor_scores = [90, 80, 70, 60, 50]
+# eng_scores = [85, 75, 65, 55, 45]
+# math_scores = [88, 78, 68, 58, 48]
+# length = len(kor_scores)
+# len_list = range(length)
+# def get_for_sum(korean_scores, english_scores, mathematics_scores):
+#     # 실행할 코드
+#     for i in range(len(korean_scores)):
+#         kor = korean_scores[i]
+#         eng = english_scores[i]
+#         math = mathematics_scores[i]
+#         sum = get_sum(kor, eng, math)
+#         print(f"{i+1}번 학생 총점: {sum}")
+#     return 0
+# get_for_sum(kor_scores, eng_scores, math_scores)
---
+++ quests/09_functions_create.py
@@ -0,0 +1,19 @@
+#문제 1번
+
+# def avg_celsius(t1, t2, t3):
+#     celsius = (t1 + t2 + t3) / 3
+#     return celsius
+# print(f"내용1: (avg_celsius(10, 20, 30))")
+# print(f"내용2: (avg_celsius(5, 15, 25))")
+# print(f"내용3: (avg_celsius(13, 23, 33))")
+
+#문제 2번
+# def Prefer(name, like1, like2):
+#     print(f"{name}님의 선호 언어는 {like1}, {like2} 입니다.")
+# Prefer("홍길동", "Python", "Java")
+# Prefer("김홍시", "magpie", "Tree")
+# Prefer("김겨울", "Snow", "Cloud")
+
+#문제 3번
+def sum_scores(scores):
+    for scores in sum_scores

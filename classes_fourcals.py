f980157e011139afab894f5d0723dabb6b2029dc
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-21 12:49:47 +0900

    3213

---
+++ classes_fourcals.py
@@ -0,0 +1,25 @@
+# class class_name():
+
+
+
+class Fourcal:
+
+    def add(self, a, b):
+        return a + b
+    def sub(self, a, b):
+        return a - b
+    def mul(self, a, b):
+        return a * b
+    def div(self, a, b):
+        if b == 0:
+            return "0으로 나눌 수 없습니다."
+        return a / b
+
+if __name__=="__main__":
+    fourcal = Fourcal()
+    print(f"3 + 5 = {fourcal.add(3, 5)}")
+    print(f"10 - 4 = {fourcal.sub(10, 4)}")
+    print(f"7 * 8 = {fourcal.mul(7, 8)}")
+    print(f"20 / 5 = {fourcal.div(20, 5)}") 
+
+pass

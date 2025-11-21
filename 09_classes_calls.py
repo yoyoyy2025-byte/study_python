007f3f144f9f5d942625c2358478818f97615add
yoyoyy2025 <yoyoyy2025@gmail.com>
2025-11-21 12:58:58 +0900

    3123

---
+++ 09_classes_calls.py
@@ -0,0 +1,10 @@
+from codes.classes_fourcals import Fourcal
+
+if __name__=="__main__":
+    fourcal = Fourcal()
+    print(f"3 + 5 = {fourcal.add(3, 5)}")
+    print(f"10 - 4 = {fourcal.sub(10, 4)}")
+    print(f"7 * 8 = {fourcal.mul(7, 8)}")
+    print(f"20 / 5 = {fourcal.div(20, 5)}") 
+
+pass
